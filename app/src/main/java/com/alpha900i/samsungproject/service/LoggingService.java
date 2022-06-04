8ac6d09cd40da2250bbc10cc3c7c882eb6edb5bd
Ivan Matkovsky <alpha900i@mail.ru>
2022-06-04 23:45:46 +0700

    Introduced service. Controlled by options menu, drops more or less pointless data into DB

---
+++ app/src/main/java/com/alpha900i/samsungproject/service/LoggingService.java
@@ -0,0 +1,81 @@
+package com.alpha900i.samsungproject.service;
+
+import static java.lang.Thread.sleep;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import com.alpha900i.samsungproject.model.AppDatabase;
+import com.alpha900i.samsungproject.model.LogEntry;
+
+import java.util.Calendar;
+import java.util.Locale;
+
+public class LoggingService extends Service {
+    private final String TAG = "LoggingService";
+    MyTask task;
+
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        task = new MyTask();
+        startWork();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        task.stop();
+    }
+
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    void startWork(){
+        Thread thread = new Thread(task,"MainThread");
+        thread.start();
+    }
+
+
+    private class MyTask implements Runnable{
+
+        private boolean serviceIsActive;
+
+        MyTask() { }
+
+        @Override
+        public void run() {
+            serviceIsActive = true;
+            int counter = 0;
+            while(serviceIsActive){
+                Calendar calendar = Calendar.getInstance();
+
+                LogEntry logEntry = new LogEntry(
+                        0, calendar.getTimeInMillis(),
+                        String.format(Locale.US, "Note %d", counter),
+                        0, false, false, false,
+                        0, 0, 0,
+                        0.0f, 0.0f, 0.0f
+                );
+                AppDatabase.getInstance(getBaseContext()).logDao().insertLog(logEntry);
+
+                Log.d(TAG, String.format(Locale.US, "Counted to %d", counter));
+                counter++;
+                try{
+                    sleep(5000);
+                } catch (InterruptedException e){
+                    e.printStackTrace();
+                    break;
+                }
+            }
+        }
+        void stop(){
+            serviceIsActive = false;
+        }
+    }
+}
--- app/src/main/java/com/alpha900i/samsungproject/view/MainActivity.java
+++ app/src/main/java/com/alpha900i/samsungproject/view/MainActivity.java
@@ -1,22 +1,14 @@
 package com.alpha900i.samsungproject.view;
 
-import android.app.ActivityManager;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-import android.os.BatteryManager;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.alpha900i.samsungproject.R;
-
-import java.util.Locale;
+import com.alpha900i.samsungproject.service.LoggingService;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -25,112 +17,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    private void formText() {
-        String batteryText = formBatteryText() + formRAMText() + formPositionText();
-        mainText.setText(batteryText);
-    }
-
-    private String formBatteryText() {
-        Context context = getBaseContext();
-
-        //current level?
-        BatteryManager bm = (BatteryManager) context.getSystemService(BATTERY_SERVICE);
-        int batLevel = bm.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
-
-        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
-        Intent batteryStatus = context.registerReceiver(null, ifilter);
-
-        //are we charging?
-        int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
-        boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
-                status == BatteryManager.BATTERY_STATUS_FULL;
-
-        //how are we charging?
-        int chargePlug = batteryStatus.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
-        boolean usbCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_USB;
-        boolean acCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_AC;
-
-        String result = String.format(Locale.US, "Battery level is %d\n" +
-                        "Charging status is %b\n" +
-                        "USB charing is %b\n" +
-                        "AC charging is %b\n" ,
-                batLevel, isCharging, usbCharge, acCharge);
-        return result;
-    }
-
-    private String formRAMText() {
-        Context context = getBaseContext();
-
-        ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
-        ActivityManager.MemoryInfo memoryInfo = new ActivityManager.MemoryInfo();
-        manager.getMemoryInfo(memoryInfo);
-
-        long divider = 1024*1024;
-        long totalRAM = memoryInfo.totalMem / divider;
-        long availRAM = memoryInfo.availMem / divider;  // remember to convert in GB,MB or KB.
-        long usedRAM = totalRAM - availRAM;
-
-        String result = String.format(Locale.US, "Available RAM is %d of %d total\n" +
-                        "%d RAM is used\n" ,
-                availRAM, totalRAM, usedRAM);
-        return result;
-    }
-
-
-    protected void onResume() {
-        super.onResume();
-
-        // Get updates from the accelerometer and magnetometer at a constant rate.
-        // To make batch operations more efficient and reduce power consumption,
-        // provide support for delaying updates to the application.
-        //
-        // In this example, the sensor reporting delay is small enough such that
-        // the application receives an update before the system checks the sensor
-        // readings again.
-        Sensor accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        if (accelerometer != null) {
-            sensorManager.registerListener(this, accelerometer,
-                    SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI);
-        }
-        Sensor magneticField = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-        if (magneticField != null) {
-            sensorManager.registerListener(this, magneticField,
-                    SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI);
-        }
-    }
-
-    private String formPositionText() {
-        final float[] rotationMatrix = new float[9];
-        SensorManager.getRotationMatrix(rotationMatrix, null,
-                accelerometerReading, magnetometerReading);
-
-        // Express the updated rotation matrix as three orientation angles.
-        final float[] orientationAngles = new float[3];
-        SensorManager.getOrientation(rotationMatrix, orientationAngles);
-
-        String result = String.format(Locale.US, "Rotation angles are\n" +
-                        "%.2f x %.2f x %.2f\n" ,
-                Math.toDegrees(orientationAngles[0]),
-                Math.toDegrees(orientationAngles[1]),
-                Math.toDegrees(orientationAngles[2]));
-        return result;
-    }
-
-
     @Override
-    public void onSensorChanged(SensorEvent event) {
-        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            System.arraycopy(event.values, 0, accelerometerReading,
-                    0, accelerometerReading.length);
-        } else if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
-            System.arraycopy(event.values, 0, magnetometerReading,
-                    0, magnetometerReading.length);
-        }
-        formText();
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
     }
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
-        ;
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.start_service_item) {
+            startService();
+            return true;
+        } else if (item.getItemId() == R.id.stop_service_item) {
+            stopService();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void startService(){
+        Intent intent = new Intent(this, LoggingService.class);
+        startService(intent);
+    }
+    public void stopService(){
+        Intent intent = new Intent(this, LoggingService.class);
+        stopService(intent);
     }
 }
