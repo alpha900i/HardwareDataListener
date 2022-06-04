245f35016a907842145efe8b4cdcadb8ce88beea
Ivan Matkovsky <alpha900i@mail.ru>
2022-06-04 23:48:10 +0700

    No point to have both "start" and "stop" options on the screen at the same time

--- app/src/main/java/com/alpha900i/samsungproject/view/MainActivity.java
+++ app/src/main/java/com/alpha900i/samsungproject/view/MainActivity.java
@@ -11,6 +11,8 @@ import com.alpha900i.samsungproject.R;
 import com.alpha900i.samsungproject.service.LoggingService;
 
 public class MainActivity extends AppCompatActivity {
+    MenuItem startServiceMenuItem, stopServiceMenuItem;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu,menu);
+        startServiceMenuItem = menu.findItem(R.id.start_service_item);
+        stopServiceMenuItem = menu.findItem(R.id.stop_service_item);
         return true;
     }
 
@@ -38,9 +42,13 @@ public class MainActivity extends AppCompatActivity {
     public void startService(){
         Intent intent = new Intent(this, LoggingService.class);
         startService(intent);
+        startServiceMenuItem.setVisible(false);
+        stopServiceMenuItem.setVisible(true);
     }
     public void stopService(){
         Intent intent = new Intent(this, LoggingService.class);
         stopService(intent);
+        startServiceMenuItem.setVisible(true);
+        stopServiceMenuItem.setVisible(false);
     }
 }
